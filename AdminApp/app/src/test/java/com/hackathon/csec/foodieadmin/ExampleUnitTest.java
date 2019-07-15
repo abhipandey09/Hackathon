4d25097737a17dd8700f82176fe7437aabc465d2
abhishek pandey <36487481+abhipandey09@users.noreply.github.com>
2019-07-15 09:50:49 +0530

    Add files via upload

---
+++ AdminApp/app/src/test/java/com/hackathon/csec/foodieadmin/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package com.hackathon.csec.foodieadmin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
